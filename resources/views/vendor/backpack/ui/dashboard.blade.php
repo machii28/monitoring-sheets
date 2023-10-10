e2394b2d874c89545d92e9cd756467261744779d
Mark Cornelio <markcornelio28@gmail.com>
2023-10-10 11:07:21 +0800

    added new fix

--- resources/views/vendor/backpack/ui/dashboard.blade.php
+++ resources/views/vendor/backpack/ui/dashboard.blade.php
@@ -1,13 +1,17 @@
 @extends(backpack_view('blank'))
 
 @php
+    $submittedSheets = \App\Models\AssignedMonitoringSheet::where('is_filled_up', false)->count();
+    $totalSheets = \App\Models\AssignedMonitoringSheet::count();
+    $progress = $totalSheets ? ($submittedSheets/$totalSheets) * 100 : 0;
+
     $widgets['after_content'][] = [
         'type'        => 'progress',
         'class'       => 'card text-white bg-primary mb-2',
-        'value'       => '11.456',
-        'description' => 'Registered users.',
-        'progress'    => 57, // integer
-        'hint'        => '8544 more until next milestone.',
+        'value'       => $submittedSheets,
+        'description' => 'Submitted Sheets',
+        'progress'    => $progress, // integer
+        'hint'        => "$totalSheets remaining un-submitted monitoring sheets",
     ];
 @endphp
 
